8530be4a032bd89a9b017fb9ffb89c84b98eb485
PSahoo95 <sahoorp1995@gmail.com>
2026-04-26 19:55:46 +0530

    feat: added a flask server code

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/pwd-generator", methods=["GET"])
+def generate_password():
+    name = request.args.get("name")
+
+    if not name:
+        return jsonify({"error": "Name parameter is required."}), 400
+    
+    pwd = f"{name}123"
+    return jsonify({"password": pwd})
+
+if __name__ == "__main__":
+    app.run(debug=True)
